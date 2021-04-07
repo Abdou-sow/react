41f325189b8556a79d12fc6f25eb56ddc1535aff
Abdou-sow <you@example.com>
2021-04-07 15:44:11 +0200

    01-03

---
+++ chapitre_01/elsy/src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import './App.css';
+
+var tempMin = -20
+var tempMax = 40
+var heartMin = 80
+var heartMax = 180
+var stepsMin = 0
+var stepsMax = 50000
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='container-fluid' >
+        <div className='row' >
+          <p>
+            Heart : 80
+            Temperature : -20
+            Steps : 0
+            </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
